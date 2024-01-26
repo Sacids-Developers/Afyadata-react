ff49b8c59b41f0be2d48c6e026c07bc056284877
mchizani <ericbeda@gmail.com>
2024-01-26 14:43:54 +0300

    added media directory

--- constants/global.js
+++ constants/global.js
@@ -5,6 +5,7 @@ export const PATH = {
 
     form_defn: FileSystem.documentDirectory + 'FORMS/DEFN/',
     form_data: FileSystem.documentDirectory + 'FORMS/DATA/',
+    form_media: FileSystem.documentDirectory + 'FORMS/DATA/MEDIA',
   
 };
 
@@ -12,6 +13,6 @@ export const PATH = {
 export const URL = {
 
     form_list: 'http://127.0.0.1:8000/form_list',
-    form_submit: 'http://127.0.0.1:8000/form_list',
+    form_submit: 'http://127.0.0.1:8000/form_submit',
   
 };
